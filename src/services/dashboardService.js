026905c82ef41c3dd57fc8c6f878d1aa509623e5
William-Unosquare <william.park@unosquare.com>
2019-01-11 12:09:25 +0000

    Update Dashboard service calls

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,22 +1,21 @@
 import axios from '../utilities/AxiosInstance';
 
 export const getUsersEvents = date => {
-  return axios.get(`/dashboard/getEmployeeEvents?date=${date}`);
+  return axios.get(`/Dashboard/getEmployeeEvents/${date}`);
 };
 
 export const getTeamsEvents = date => {
-  return axios.get(`/dashboard/getTeamEvents?date=${date}`);
+  return axios.get(`/Dashboard/getTeamEvents/${date}`);
 };
 
 export const getEmployeesTeamsSnapshot = () => {
-  return axios.get('/dashboard/getEmployeeTeamSnapshot');
+  return axios.get('/Dashboard/getEmployeeTeamSnapshot');
 };
 
 export const getTeamOverview = () => {
-  return axios.get('/dashboard/getDashboardSnapshot');
+  return axios.get('/Dashboard/getDashboardSnapshot');
 };
 
 export const getMessagesByEventId = eventId => {
-  return axios.get(`/dashboard/getMessagesByEventId/${eventId}`);
+  return axios.get(`/Dashboard/getMessagesByEventId/${eventId}`);
 };
-
