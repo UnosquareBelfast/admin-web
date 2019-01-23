264f4b6c0fe9acd6526f74d81454db21ce739642
William-Unosquare <william.park@unosquare.com>
2019-01-23 14:45:00 +0000

    Remove old auth code

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,16 +1,4 @@
-import decode from 'jwt-decode';
 import axios from '../utilities/AxiosInstance';
-
-export const userLogin = (email, password) => {
-  return axios
-    .post('authentication/login', { email, password })
-    .then(response => {
-      const token = response.data.accessToken;
-      let userData = decode(token);
-      localStorage.setItem('id_token', token);
-      localStorage.setItem('user_id', userData.sub);
-    });
-};
 
 export const getSignedInUser = () => {
   return axios.get('/Employee/getSignedInUser');
--- src/utilities/currentUser.js
+++ src/utilities/currentUser.js
@@ -1,15 +1,6 @@
-import decode from 'jwt-decode';
 import store from '../store';
 import { resetUser } from '../actions/user';
 import AzureInstance from '../utilities/AzureInstance';
-
-export const getProfile = () => {
-  return decode(localStorage.getItem('id_token'));
-};
-
-export const isLoggedIn = () => {
-  return !!localStorage.getItem('msal.idtoken');
-};
 
 export const userLogout = () => {
   store.dispatch(resetUser());
