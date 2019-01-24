4428a4dc3321b4b0ef25e7923f83a2bc15eddf87
William-Unosquare <william.park@unosquare.com>
2019-01-24 11:47:55 +0000

    Fix token expiration interceptor

--- src/utilities/AxiosInstance.js
+++ src/utilities/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import moment from 'moment';
 import store from '../store';
+import { resetUser } from '../actions/user';
 
 const baseURL = process.env.DOMAIN;
 const instance = axios.create({
@@ -14,17 +14,22 @@ instance.interceptors.request.use(function(config) {
   return config;
 });
 
-instance.interceptors.response.use(function(response) {
-  // If unauthorized status gets returned from a call, log the user out.
-  // The token has likely expired.
+instance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    const { response } = error;
 
-  if (response.status === 401) {
-    localStorage.clear();
-    localStorage.setItem('ac-prev-expired', true);
-    location.reload();
+    // If unauthorized status gets returned from a call, log the user out.
+    // The token has likely expired.
+    if (response.status === 401) {
+      localStorage.clear();
+      store.dispatch(resetUser);
+      localStorage.setItem('ac-prev-expired', true);
+      location.reload();
+    }
   }
-
-  return response;
-});
+);
 
 export default instance;
