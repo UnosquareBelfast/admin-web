08b06e8e11bfb655ac4d25aa4796dc6a7206bf66
William-Unosquare <william.park@unosquare.com>
2019-01-25 12:09:40 +0000

    Admin dashboard refresh and remove old admin option

--- src/components/AdminDashboard/styled.js
+++ src/components/AdminDashboard/styled.js
@@ -1,48 +1,48 @@
 import styled from 'styled-components';
 
 export const InnerLayout = styled.div`
-  width: 100%;
-  h2 {
-    margin: 0;
-  }
+      h3 {
+        padding: 15px;
+        padding-bottom: 0;
+      }
+      a {
+          padding: 15px;
+          border-bottom: 1px solid ${({ theme }) => theme.colours.grey};
+          text-decoration: none;
+          color: ${({ theme }) => theme.colours.unoBlue};
+          font-weight: bold;
+
+          :first-of-type {
+            border-top: 1px solid ${({ theme }) => theme.colours.grey};
+          }
+
+          :last-of-type {
+            border-bottom: none;
+          }
+
+          :hover {
+            color: white;
+            background: ${({ theme }) => theme.colours.unoBlue};
+            border: 0;
+          }
+        }
+      }
+    }
+  } */}
 
   .columnWrap {
     display: flex;
-    margin-top: 20px;
-    border-radius: 8px;
+    flex-wrap: wrap;
 
-    div {
+    > div {
+      padding: 0px;
+      margin: 15px;
       display: flex;
       flex-direction: column;
-      align-items: center;
-      padding: 20px 10px;
-      width: 100%;
+      box-shadow: 0px 2px 10px 0px rgba(0,0,0,0.25);
       border: 1px solid ${({ theme }) => theme.colours.grey};
-      border-right: none;
-
-      :first-of-type {
-        border-radius: 8px 0 0 8px;
-      }
-      :last-of-type {
-        border-radius: 0 8px 8px 0;
-        border-right: 1px solid ${({ theme }) => theme.colours.grey};
-      }
-
-      h3 {
-        margin: 0 0 10px 0;
-      }
-
-      a {
-        color: ${({ theme }) => theme.colours.unoBlue};
-        font-weight: ${props => props.theme.fonts.weight.bold};
-        text-decoration: none;
-        margin-bottom: 5px;
-
-        :hover {
-          color: ${({ theme }) => theme.colours.darkBlue};
-          text-decoration: underline;
-        }
-      }
+      border-radius: 4px;
+      width: 400px;
     }
   }
 `;
