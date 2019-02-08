929604b8082448b7a88e8f213499b2a26eee42d1
William Park <william.park@unosquare.com>
2019-02-08 10:46:36 +0000

    Fix filtering on client tables

--- src/components/DataTable/Cells/clients.js
+++ src/components/DataTable/Cells/clients.js
@@ -2,6 +2,7 @@ const clientName = {
   id: 'clientName',
   Header: 'Client',
   accessor: client => client.clientName,
+  filterMethod: ({ value }, { clientName }) => clientName.toLowerCase().includes(value.toLowerCase()),
 };
 
 export default {
