e2d9685d9f0c23bb0a8640c9f14f862fb4f2d265
William Park <william.park@unosquare.com>
2019-02-08 10:28:28 +0000

    Fix case sensitivity on user tables

--- src/components/DataTable/Cells/users.js
+++ src/components/DataTable/Cells/users.js
@@ -2,36 +2,42 @@ const fullName = {
   id: 'fullName',
   Header: 'Full Name',
   accessor: user => `${user.forename} ${user.surname}`,
+  filterMethod: ({ value }, { fullName }) => fullName.toLowerCase().includes(value.toLowerCase()),
 };
 
 const name = {
   id: 'name',
   Header: 'Name',
   accessor: user => user.name || `${user.forename} ${user.surname}`,
+  filterMethod: ({ value }, { name }) => name.toLowerCase().includes(value.toLowerCase()),
 };
 
 const email = {
   id: 'email',
   Header: 'Email',
   accessor: user => user.email,
+  filterMethod: ({ value }, { email }) => email.toLowerCase().includes(value.toLowerCase()),
 };
 
 const location = {
   id: 'location',
   Header: 'Location',
   accessor: user => user.countryDescription,
+  filterMethod: ({ value }, { location }) => location.toLowerCase().includes(value.toLowerCase()),
 };
 
 const role = {
   id: 'role',
   Header: 'Role',
   accessor: user => user.employeeRoleDescription,
+  filterMethod: ({ value }, { role }) => role.toLowerCase().includes(value.toLowerCase()),
 };
 
 const state = {
   id: 'state',
   Header: 'Today\'s status',
   accessor: user => user.location,
+  filterMethod: ({ value }, { state }) => state.toLowerCase().includes(value.toLowerCase()),
 };
 
 export default {
