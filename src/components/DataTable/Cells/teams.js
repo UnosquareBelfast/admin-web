e7b49c6006559d839a4b49ca8273781f15dc3f91
William Park <william.park@unosquare.com>
2019-02-08 10:47:21 +0000

    Fix case sensitive filtering on team tables

--- src/components/DataTable/Cells/teams.js
+++ src/components/DataTable/Cells/teams.js
@@ -2,6 +2,7 @@ const teamName = {
   id: 'teamName',
   Header: 'Team Name',
   accessor: team => team.teamName,
+  filterMethod: ({ value }, { teamName }) => teamName.toLowerCase().includes(value.toLowerCase()),
 };
 
 export default {
