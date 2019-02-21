4932b77d0249435668cbfd0a55b0d6271f263b7e
William Park <william.park@unosquare.com>
2019-02-21 12:00:20 +0000

    Remove old code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Login } from './Login';
 export { default as Admin } from './Admin';
-export { default as User } from './User';
 export { default as Profile } from './Profile';
 export { default as TeamDashboard } from './TeamDashboard';
 export { default as Register } from './Register';
