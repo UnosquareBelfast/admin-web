f6f809f71c8b5c52ade6e855f95efd62d089409a
Paul Matchett <paul.matchett@unosquare.com>
2019-02-22 09:31:10 +0000

    removed Formand Input component and replaced with Formik

--- src/components/CreateTeam/CreateTeamForm/index.jsx
+++ src/components/CreateTeam/CreateTeamForm/index.jsx
@@ -1,64 +1,108 @@
 import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import container from './container';
-import { Form, Input } from '../../common';
+
 import { FormContainer } from './styled';
+import { FormStyleContainer } from '../../common_styled/FormStyleContainer';
+import { withFormik } from 'formik';
+
+
+const FormikEnhancer = withFormik({
+  displayName: 'Create Team Form',
+
+  // This updates the form when props change.
+  enableReinitialize: true,
+
+  // validateOn
+  mapPropsToValues: () => ({
+    selectedClient: -1,
+    teamName: '',
+  }),
+
+  // Custom sync validation
+  validate: ({ teamName, selectedClient }) => {
+    let errors = {};
+    if (teamName === '') {
+      errors.teamName = 'Team name cannot be empty.';
+    }
+    if (selectedClient === '-1') {
+      errors.selectedClient = 'Please select a client';
+    }
+    return errors;
+  },
+
+  handleSubmit: (payload, bag) => {
+    bag.props.handleFormSubmit(payload);
+  },
+
+});
 
 export const CreateTeamForm = props => {
-  const { submitForm, formStatus, formData, formIsValid, clients } = props;
+  const { handleSubmit, handleChange, handleBlur, clients, errors, isValid, touched, values } = props;
 
   return (
     <FormContainer>
-      <Form
-        formData={formData}
-        formStatus={formStatus}
-        actions={[
-          {
-            label: 'Create Team',
-            event: submitForm,
-            disabled: !formIsValid || formData.selectedClient == -1,
-          },
-        ]}
-      >
-        <Input
-          label="Client:"
-          type="select"
-          htmlAttrs={{
-            name: 'selectedClient',
-            options: clients,
-          }}
-          value={formData.selectedClient}
-        />
-        <Input
-          label="Team Name:"
-          type="input"
-          htmlAttrs={{
-            type: 'input',
-            name: 'teamName',
-            placeholder: 'Enter a team name',
-            disabled: formData.selectedClient == -1,
-          }}
-          value={formData.teamName}
-          rules={{
-            required: true,
-            minLength: 3,
-          }}
-        />
-      </Form>
+      <FormStyleContainer>
+        <form onSubmit={handleSubmit}>
+
+          <div className={errors.selectedClient && touched.selectedClient ? 'formgroup formgroup--invalid' : 'formgroup' }>
+            <label htmlFor="selectedClient">Select a Client</label>
+            <select
+              id="selectedClient"
+              name="selectedClient"
+              value={values.selectedClient}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            >
+              {
+                clients.map((client) => {
+                  const {value, displayValue} = client;
+                  return (
+                    <option key={value} value={value}>{displayValue}</option>
+                  );
+                })
+              }
+            </select>
+            <span>{errors.selectedClient}</span>
+          </div>
+
+          <div className={errors.teamName && touched.teamName ? 'formgroup formgroup--invalid' : 'formgroup' }>
+            <label htmlFor="teamname">Team Name</label>
+            <input
+              type="text"
+              id="teamName"
+              name="teamName"
+              placeholder="Enter a Team name"
+              value={values.teamName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <span>{errors.teamName}</span>
+          </div>
+
+          <button type="submit" disabled={!isValid}>
+            Submit
+          </button>
+
+        </form>
+      </FormStyleContainer>
     </FormContainer>
   );
 };
 
 CreateTeamForm.propTypes = {
   clients: PT.array,
-  formData: PT.object.isRequired,
-  submitForm: PT.func.isRequired,
-  formStatus: PT.func.isRequired,
-  formIsValid: PT.bool.isRequired,
+  handleSubmit: PT.func.isRequired,
+  handleChange: PT.func.isRequired,
+  handleBlur: PT.func.isRequired,
+  isValid: PT.bool.isRequired,
+  errors: PT.object.isRequired,
+  touched: PT.object.isRequired,
+  values: PT.object.isRequired,
 };
 
 CreateTeamForm.defaultProps = {
   clients: [],
 };
 
-export default container(CreateTeamForm);
+export default container(FormikEnhancer(CreateTeamForm));
