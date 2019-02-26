12e7c50ac6d6e1ff79624dbf2448f73d8c2fb51b
Paul Matchett <paul.matchett@unosquare.com>
2019-02-26 14:25:55 +0000

    Updated list of holidays to be pending first, in date order

--- src/components/AllHolidays/container.js
+++ src/components/AllHolidays/container.js
@@ -13,9 +13,32 @@ export default Wrapped =>
     }
 
     getHolidays = () => {
-      getAllEvents().then(response => {
-        const holidays = response.data;
+      getAllEvents().then( ({data: allHolidays}) => {
+
+        // sort holidays by start date
+        allHolidays.sort( (a, b) => {
+          let dateOne = a.eventDates[0].startDate;
+          let dateTwo = b.eventDates[0].startDate;
+          return new Date(dateOne) - new Date(dateTwo);
+        });
+
+        // split up pending holdays from all holidays 
+        let pendingHolidays = [];
+        let nonPendingHolidays = [];
+        allHolidays.map((holiday) => {
+          if ( holiday.eventStatus.eventStatusId === 1 ) {
+            pendingHolidays.push(holiday);
+          } else {
+            nonPendingHolidays.push(holiday);
+          }
+        });
+
+        // add pending holidays to start of array, order by date
+        const holidays = [...pendingHolidays, ...nonPendingHolidays];
+
+        // update the holidays state
         this.setState({ holidays });
+
       });
     };
 
