f892d81b2c49654e35dc219932ec6d140202c80b
Paul Matchett <paul.matchett@unosquare.com>
2019-03-05 12:12:31 +0000

    Added HOC for label and error message

---
+++ src/components/common/Formik/formgroup.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { PropTypes as PT } from 'prop-types';
+
+import { InputGroup, Label, Error } from './styled';
+
+const FormGroup = (props) => {
+
+  const { 
+    layout,
+    title, 
+    name, 
+    error, 
+    children,
+  } = props;
+  
+  const formatCss = () => {    
+
+    let formGroupCss = [];
+    const containsCheckbox = layout.includes('contains-checkbox');
+
+    if (containsCheckbox) {
+      formGroupCss.push(layout);
+    }
+
+    if (error !== '') {
+      formGroupCss.push('invalid');
+    }
+
+    return formGroupCss.join(' ');
+  };
+
+  return (
+    <InputGroup
+      className={formatCss()}
+    >
+      <Label htmlFor={name}>{title}</Label>
+      {children}
+      {
+        error !== '' &&
+        <Error>{error}</Error>
+      }
+    </InputGroup>
+  );
+};
+
+FormGroup.propTypes = {
+  layout: PT.string,
+  title: PT.string, 
+  name: PT.string,
+  children: PT.node,
+  error: PT.string, 
+};
+
+FormGroup.defaultProps = {
+  layout: 'block',
+  error: '',
+};
+
+export default FormGroup;
