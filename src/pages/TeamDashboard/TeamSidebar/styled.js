11d0527ccd596913d4a11de944d39f6feab650fa
Paul Matchett <paul.matchett@unosquare.com>
2019-03-06 08:28:46 +0000

    Added animation and arrow to active team link

--- src/pages/TeamDashboard/TeamSidebar/styled.js
+++ src/pages/TeamDashboard/TeamSidebar/styled.js
@@ -15,6 +15,7 @@ export const ContainerStyle = styled.div`
   }
 
   .team-link {
+    position: relative;
     margin: 0 0 5px 0;
     color: ${props => props.theme.colours.unoBlue};
     background-color: transparent;
@@ -26,17 +27,43 @@ export const ContainerStyle = styled.div`
     user-select: none;
     transition: background-color 500ms;
 
+    &::after {
+      transition: all 0s ease 0s;
+      display: block;
+      content: '';
+      position: absolute;
+      right: -20px;
+      top: -2px;
+      height: 0;
+      width: 0;
+      background-color: transparent;
+      border-style: solid;
+      border-width: 17px 0 17px 20px;
+      border-color: transparent transparent transparent transparent;
+    }
+
     &:first-of-type {
       margin-top: 32px;
     }
 
     &:hover {
-      background-color: ${props => props.theme.colours.white};
+      background-color: ${props => props.theme.colours.unoBlue};
+      color: ${props => props.theme.colours.white};
+      opacity: 0.6;
     }
 
     &.active {
       background-color: ${props => props.theme.colours.unoBlue};
       color: ${props => props.theme.colours.white};
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+      opacity: 1;
+
+      ::after {
+        transition: all 200ms linear 200ms;
+        right: -22px;
+        border-color: transparent transparent transparent ${props => props.theme.colours.unoBlue};
+      }
     }
 
   }
