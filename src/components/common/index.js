54220777d67019a89e7339d88f89d860a5e96509
Paul Matchett <paul.matchett@unosquare.com>
2019-03-13 08:08:33 +0000

    removed react-day-picker and UserDetails from project

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,7 +2,6 @@ export { default as Card } from './Card';
 export { default as Button } from './Button';
 export { default as ToggleButton } from './ToggleButton';
 export { default as Modal } from './Modal';
-export { default as RangeDayPicker } from './RangeDayPicker';
 export { default as Errorbox } from './Errorbox';
 export { default as Event } from './Event';
 export { default as Input } from './Input';
