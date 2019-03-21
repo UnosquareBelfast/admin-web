957b42c78aaadc1b4e16a0a00c65b403258edc30
Paul Matchett <paul.matchett@unosquare.com>
2019-03-21 14:08:25 +0000

    Connected CreateClient to clients redux

--- src/components/CreateClient/container.js
+++ src/components/CreateClient/container.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { PropTypes as PT } from 'prop-types';
-import { createClient } from '../../services/clientService';
-import { Toast } from '../../config/Notifications';
-import swal from 'sweetalert2';
+import { postNewClient } from '../../store/actions/clients';
 
-export default Wrapped =>
+const CreateClientContainer = Wrapped =>
   class extends Component {
     static propTypes = {
+      postNewClient: PT.func.isRequired,
       history: PT.object,
       match: PT.object,
     };
 
     submitRequest = (data, resetForm) => {
-      createClient(data)
-        .then(() => {
-          resetForm();
-          Toast({ 
-            type: 'success', 
-            title: `${data.clientName} created sucessfully! 👍`, 
-          });
-        })
-        .catch(error =>
-          swal('Error', `Error creating ${data.clientName}:${error.message}`, 'error')
-        );
+      this.props.postNewClient(data, resetForm);
     };
 
     render() {
@@ -35,3 +26,12 @@ export default Wrapped =>
       );
     }
   };
+
+const mapDispatchToProps = dispatch => {
+  return {
+    postNewClient: (data, resetForm) =>
+      dispatch(postNewClient(data, resetForm)),
+  };
+};
+
+export default compose(connect(null, mapDispatchToProps), CreateClientContainer);
