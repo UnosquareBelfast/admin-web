83a8c5825b4364db051a1893a0553ff50c8043d9
Paul Matchett <paul.matchett@unosquare.com>
2019-03-21 14:20:22 +0000

    Added employee and teams list from redux for filtering

--- src/components/ViewContracts/container.js
+++ src/components/ViewContracts/container.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { PropTypes as PT } from 'prop-types';
-import { Toast } from '../../config/Notifications';
+import { getAllEmployees, getTeamsOptions } from '../../store/reducers';
 import swal from 'sweetalert2';
 
-import { getUserByName } from '../../services/userService';
-import { getContractsByEmployeeId } from '../../services/contractService';
+import {
+  getContractsByEmployeeId,
+  getContractsByTeamId,
+  getContractByEmployeeIdAndTeamId,
+} from '../../services/contractService';
 
-export default Wrapped =>
+const ViewContractsContainer = Wrapped =>
   class extends Component {
 
     static propTypes = {
+      employees: PT.array,
+      teams: PT.object,
       history: PT.shape({
         push: PT.func.isRequired,
       }),
@@ -18,13 +25,25 @@ export default Wrapped =>
     constructor(props) {
       super(props);
       this.state = {
-        users: [],
+        employeesOptions: [],
+        selectedEmployeeId: -1,
+        selectedTeamId: -1,
         contracts: [],
       };
     }
 
-    getContracts = (selectedUserId) => {
-      getContractsByEmployeeId(selectedUserId)
+    _byEmployeeId = (selectedEmployeeId) => {
+      getContractsByEmployeeId(selectedEmployeeId).then(response => {
+        const contracts = response.status === 200 ? response.data : [];
+        this.setState({ contracts });
+      })
+        .catch(error =>
+          swal('Error'`Error finding contracts: ${error.message}`, 'error')
+        );
+    }
+
+    _byTeamId = (selectedTeamId) => {
+      getContractsByTeamId(selectedTeamId)
         .then(response => {
           const contracts = response.status === 200 ? response.data : [];
           this.setState({ contracts });
@@ -32,53 +51,95 @@ export default Wrapped =>
         .catch(error =>
           swal('Error'`Error finding contracts: ${error.message}`, 'error')
         );
+    }
+
+    _byEmployeeIdAndTeamId = (selectedEmployeeId, selectedTeamId) => {
+      getContractByEmployeeIdAndTeamId(selectedEmployeeId, selectedTeamId)
+        .then(response => {
+          const contracts = response.status === 200 ? response.data : [];
+          this.setState({ contracts });
+        })
+        .catch(error =>
+          swal('Error'`Error finding contracts: ${error.message}`, 'error')
+        );
+    }
+
+    getContracts = (name, id) => {
+
+      const obj = {};
+      obj[name] = parseInt(id);
+
+      this.setState(obj, () => {
+
+        const { selectedEmployeeId, selectedTeamId } = this.state;
+
+        const searchByEmployees = selectedEmployeeId !== -1 && selectedTeamId === -1;
+        const searchByTeams = selectedTeamId !== -1 && selectedEmployeeId === -1;
+        const searchByBoth = selectedTeamId !== -1 && selectedEmployeeId !== -1;
+
+        if (searchByEmployees) {
+          this._byEmployeeId(selectedEmployeeId);
+        }
+        else if (searchByTeams) {
+          this._byTeamId(selectedTeamId);
+        }
+        else if (searchByBoth) {
+          this._byEmployeeIdAndTeamId(selectedEmployeeId, selectedTeamId);
+        } else {
+          this.setState({ contracts: [] });
+        }
+      });
+
     };
 
-    getUsers = ({ fullName }) => {
-      const fullNameArray = fullName.split(' ');
-      const forename = fullNameArray[0];
-      const surname = fullNameArray[1];
-      getUserByName(forename, surname).then(response => {
-        const users = response.data;
-        Toast({
-          type: 'success',
-          title: `${users.length} employee${users.length > 1 ? 's' : ''} found! 👍`,
+    getEmployees = ({ fullName }) => {
+
+      const employees = this.props.employees.filter(({ forename, surname }) => {
+        const fullname = `${forename} ${surname}`;
+        return fullname.includes(fullName);
+      });
+
+      const employeesOptions = employees.reduce((acc, employee) => {
+        acc.push({
+          value: employee.employeeId,
+          displayValue: `${employee.forename} ${employee.surname} (${employee.email})`,
         });
-        const formattedUsers = users.reduce((acc, user) => {
-          acc.push({
-            value: user.employeeId,
-            displayValue: `${user.forename} ${user.surname} (${user.email})`,
-          });
-          return acc;
-        }, []);
-        formattedUsers.unshift({
-          value: '-1',
-          displayValue: 'Please select an employee',
-        });
-        this.setState(
-          {
-            users: formattedUsers,
-          }
-        );
-      })
-        .catch(() =>
-          swal(
-            'Error',
-            `Could not retreive contracts for ${fullName}. Please try again`,
-            'error',
-          ),
-        );
+        return acc;
+      }, []);
+      employeesOptions.unshift({
+        value: '-1',
+        displayValue: 'Please select an employee',
+      });
+      this.setState(
+        {
+          employeesOptions,
+        }
+      );
     };
 
     render() {
+
+      const { history, employees, teams } = this.props;
+
       return (
         <Wrapped
-          navigateTo={this.props.history.push}
-          users={this.state.users}
+          navigateTo={history.push}
+          employeeCount={employees.length}
+          teamOptions={teams.teamOptions}
+          employeesOptions={this.state.employeesOptions}
           contracts={this.state.contracts}
-          getUsers={this.getUsers}
+          getEmployees={this.getEmployees}
           contractSearch={this.getContracts}
         />
       );
     }
   };
+
+const mapStateToProps = state => {
+  return {
+    employees: getAllEmployees(state),
+    teams: getTeamsOptions(state),
+  };
+};
+
+export default compose(connect(mapStateToProps), ViewContractsContainer);
--- src/components/ViewContracts/styled.js
+++ src/components/ViewContracts/styled.js
@@ -3,8 +3,21 @@ import styled from 'styled-components';
 export const FormContainer = styled.div`
   form {
     width: 100%;
-    @media (min-width: ${props => props.theme.mediaQueries.lg}) {
-      width: 50%;
-    }
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-content: space-between;
+    align-items: flex-end;
   }
 `;
+
+export const Column = styled.div`
+  flex: 1 1 auto;
+  margin-left: 20px;
+  align-self: auto;
+  width: 50%;
+  &:first-child {
+    margin-left: 0;
+  }
+`;     
